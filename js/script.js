1af0b7ac0eae150770543516ce8b1a8ed41bcf64
devCKVargas <50130192+devCKVargas@users.noreply.github.com>
2023-06-02 17:49:13 +0800

    CSS: Fix unwanted circumstance Refactor: JavaScript & added auto toggleOverlay

--- js/script.js
+++ js/script.js
@@ -1,31 +1,57 @@
-// const navToggle = document.querySelector(".nav--button--menu");
-// navToggle.addEventListener("click", () => {
-// 	// navToggle.classList.toggle("opened");
-// 	console.log("haha");
-// });
-
-// document
-// 	.querySelector(".nav--menu--button")
-// 	.addEventListener("click", function () {
-// 		document.querySelector(".navlist").classList.add("opened");
-// 	});
-
 "use strict";
 
-document.querySelector(".nav--menu--button").addEventListener("click", () => {
-	document.querySelector(".nav--list").classList.toggle("opened");
-	document.querySelector(".nav--close--button").classList.toggle("closed");
-	document.querySelector(".overlay").style.display = "block";
+//? HTML Elements
+const navMenuBtn = document.querySelector(".nav--menu--button");
+const navList = document.querySelector(".nav--list");
+const navListAnchorTags = document.querySelectorAll(".nav--list a");
+const navCloseBtn = document.querySelector(".nav--close--button");
+const overlay = document.querySelector(".overlay");
+
+//? Check if overlay is open
+let overlayIsOpen = window.getComputedStyle(overlay).display === "block";
+
+//? Function
+const toggleOverlay = () => {
+	overlay.style.display = "none";
+	navCloseBtn.classList.toggle("closed");
+	navList.classList.toggle("opened");
+	overlayIsOpen = false;
+};
+
+const closeOverlay = () => {
+	overlayIsOpen === true ? toggleOverlay() : null;
+};
+
+//? Close overlay when user clicks a navLink
+for (const links of navListAnchorTags) {
+	links.addEventListener("click", closeOverlay);
+}
+
+//? Dynamically get Viewport Width & Height
+window.addEventListener(`resize`, () => {
+	const vw = Math.max(
+		document.documentElement.clientWidth || 0,
+		window.innerWidth || 0
+	);
+	const vh = Math.max(
+		document.documentElement.clientHeight || 0,
+		window.innerHeight || 0
+	);
+
+	//? Close overlay if viewWidth reaches 700
+	vw >= 700 ? closeOverlay() : null;
 });
 
-document.querySelector(".nav--close--button").addEventListener("click", () => {
-	document.querySelector(".nav--list").classList.toggle("opened");
-	document.querySelector(".nav--close--button").classList.toggle("closed");
-	document.querySelector(".overlay").style.display = "none";
+//? onclick Overlay
+overlay.addEventListener("click", toggleOverlay);
+
+//? onclick Menu Btn
+navMenuBtn.addEventListener("click", () => {
+	navList.classList.toggle("opened");
+	navCloseBtn.classList.toggle("closed");
+	overlay.style.display = "block";
+	overlayIsOpen = true;
 });
 
-document.querySelector(".overlay").addEventListener("click", () => {
-	document.querySelector(".overlay").style.display = "none";
-	document.querySelector(".nav--close--button").classList.toggle("closed");
-	document.querySelector(".nav--list").classList.toggle("opened");
-});
+//? onclick Close Btn
+navCloseBtn.addEventListener("click", toggleOverlay);
